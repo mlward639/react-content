a21ec76b0c8c815971e6d81bd522d622f2add8ab
Maggie Ward <mlward639@gmail.com>
2021-08-15 00:57:57 -0400

    unit 20 act 1 content

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// The ReactDOM.render method is used to render a react element into the actual DOM
+// The first argument is the component we want to render, and the second is the container element on the page
+ReactDOM.render(<App />, document.getElementById('root'));
