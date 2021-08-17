de25cde7dcfa6bd1dcb0862331f7f6d157faa18a
Maggie Ward <mlward639@gmail.com>
2021-08-17 10:48:00 -0400

    unit20 act 22

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from "react";
-import List from "./components/List";
-
-const groceries = [
-  {
-    id: 1,
-    name: "Milk",
-    purchased: true
-  },
-  {
-    id: 2,
-    name: "Eggs",
-    purchased: true
-  },
-  {
-    id: 3,
-    name: "Cheese",
-    purchased: false
-  },
-  {
-    id: 4,
-    name: "Cake Mix",
-    purchased: false
-  },
-  {
-    id: 5,
-    name: "Carrots",
-    purchased: false
-  },
-  {
-    id: 6,
-    name: "Juice",
-    purchased: true
-  }
-];
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <List groceries={groceries} />;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Section />
+    </div>
+  );
 }
 
 export default App;
