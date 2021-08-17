3fa37e104bfacf9a30700b12ff798f53562a3aeb
Maggie Ward <mlward639@gmail.com>
2021-08-16 22:07:31 -0400

    unit20 act2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Thermostat from './components/Thermostat';
+// TODO: Add a comment explaining what this import statement is doing
+import HelloReact from './components/HelloReact';
 
+// TODO: Add a comment explaining the purpose of the App component
 function App() {
-  return <Thermostat />;
+  return <HelloReact />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// TODO: Add a comment giving a brief description of what React is
+import React from 'react';
 
+// TODO: Add a comment that describes the difference between react and react-dom
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+
+// TODO: Add a comment describing the significance of the ReactDOM.render method
+ReactDOM.render(<App />, document.getElementById('root'));
